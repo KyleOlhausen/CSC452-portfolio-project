4557c118da582d182aeaf117e3060a61f90ca08d
KyleOlhausen <olhausen.kyle43@gmail.com>
2023-07-23 14:37:27 -0400

    added hamburger menu to nav

--- main.js
+++ main.js
@@ -14,3 +14,13 @@ const observer = new IntersectionObserver(entries => {
 hiddenElements.forEach(element => {
     observer.observe(element)
 })
+
+
+//nav hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
