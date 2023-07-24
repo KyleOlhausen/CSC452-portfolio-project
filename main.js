e628a63454cdd8867599de35e7e30e3da27276d0
KyleOlhausen <olhausen.kyle43@gmail.com>
2023-07-23 21:44:50 -0400

    added animation, favicon, and organized code

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
 //Nav hamburger menu
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
 })
 
-document.querySelectorAll(".nav-link").forEach(link => link.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach(link => link.addEventListener("click", () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
 }))
 
 
-//Card animation
-const hiddenElements = document.querySelectorAll('.hidden');
+//Animation
+const hiddenElements = document.querySelectorAll('.hidden-card, .hidden-title, .hidden-abt, .hidden-header-name');
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        entry.target.classList.toggle("show", entry.isIntersecting)
+        if(entry.target.classList.contains('card')){
+            entry.target.classList.toggle("show-card", entry.isIntersecting)
+        }
+        else if (entry.target.classList.contains('about-card')){
+            entry.target.classList.toggle("show-abt-card", entry.isIntersecting)
+        }
+        else if (entry.target.classList.contains('hero-container')){
+            entry.target.classList.toggle("show-title", entry.isIntersecting)
+        }        
     })
 })
 
 hiddenElements.forEach(element => {
     observer.observe(element)
 })
-
-
